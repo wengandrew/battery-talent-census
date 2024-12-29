b40db23abb41fb046fcdf4696cdd0a3c8f011751
Andrew Weng <weng.andrew@gmail.com>
2024-12-28 22:04:37 -0500

    New prompts for more specific tasks

--- src/llm.py
+++ src/llm.py
@@ -5,32 +5,9 @@ Use LLMs to analyze free-form text responses from the Census.
 
 import openai
 import json
+from textwrap import dedent
 
 from dotenv import load_dotenv
-
-SYSTEM_PROMPT = """
-
-You are an expert data analyst.
-
-You are given a list of strings that represent survey responses from multiple respondents. Each element of the list contains the response from a single respondent. Each respondent is allowed give a maximum of three answers, e.g., ["battery manufacturing", "communication", "coding skills"].
-
-Your goal is the following.
-
-1. Define relevant categories that holistically capture the range of answers from all the respondents.
-2. For each category defined, count the number of respondents that gave an answer that fits the category.
-3. Store the keywords from the responses that fit each category.
-4. Return all responses only as a JSON with the following format:
-{{
-    categories: [
-        {{
-            "category": category name,
-            "count": number of occurrences,
-            "keywords": ["list", "of", "keywords"]
-        }}
-    ]
-}}
-
-"""
 
 class LLM:
 
@@ -41,10 +18,18 @@ class LLM:
         self.client = openai.Client()
 
 
-    def analyze(self, list_of_strings,
+    def analyze_one_shot(self, list_of_strings,
                       model='o1-preview'):
         """
-        Analyze the list of strings using the LLM
+        Analyze the list of strings in one shot.
+
+        This is the most difficult problem for the LLM to solve.
+
+        It requires the LLM to interpret a list of strings where each string contains multiple keywords that are delimited in different ways; then the LLM needs to define broad (but not too broad) categories that capture the range of responses and count the number of responses that fit each category.
+
+        There are at least three steps here that the LLM is asked to do in one shot.
+
+        It's not working very well as of 12/28/2024. The LLM returns responses but they are not repeatable from run-to-run.
 
         Outputs
         -------
@@ -55,8 +40,24 @@ class LLM:
 
         """
 
+        system_prompt = """
+
+        You are an expert data analyst for a survey company.  You have been given a list of survey responses and asked to categorize them.  You are to define relevant categories that holistically capture the range of responses and count the number of responses that fit each category.
+
+        Return all responses only as a JSON with the following format:
+        {{
+            categories: [
+                {{
+                    "category": category name
+                    "count": number of occurrences
+                }}
+            ]
+        }}
+
+        """
+
         prompt = f"""
-                  {SYSTEM_PROMPT}
+                  {system_prompt}
                   {str(list_of_strings)}
                   """
 
@@ -113,3 +114,145 @@ class LLM:
         counter_dict['_tot_'] = len(list_of_strings)
 
         return output_dict, counter_dict
+
+
+    def define_categories(self, question, keyword_list):
+        """
+        Define categories for a list of keywords.
+
+        Parameters
+        ----------
+        question : str
+            question to ask the LLM
+        keyword_list : list
+            list of keywords
+
+        Returns
+        -------
+        output_dict : dict
+            output from the LLM
+        """
+
+        sys_prompt = f"""
+        You are a helpful text analyzer.
+
+        I will provide you with a list of survey answers collected from a census.
+
+        Survey takers are asked to answer the following question: '{question}'.
+
+        Your task is to define specific categories that capture all of the answers.
+
+        Avoid defining categories that are too vague or general. For example, instead of defining a category called 'technical skills', analyze the different types of skills listed to break them out into specific domains such as 'battery chemistry', 'battery engineering', 'battery testing', etc. Similarly, instead of defining a category called 'soft skills', analyze the different types of soft skills listed to break them out into specific domains such as 'communication', 'teamwork', 'leadership', etc.
+
+        You can define up to but not more than 20 categories.
+
+        For each category, list the survey answers that belong to the category. Return your solution only as a JSON with the following format:
+
+        {{
+            categories: [
+                {{
+                "name" : category name
+                "keywords" : list of user responses that belong to this category
+                }}
+            ]
+        }}
+        """
+
+        response = self.client.chat.completions.create(
+            model='o1-preview',
+            messages=[
+                {'role': 'user', 'content': dedent(sys_prompt) + '\n\n' + str(keyword_list)},
+            ],
+        )
+
+        content = response.choices[0].message.content.\
+            replace('```json\n', '').\
+            replace('```', '')
+
+        output_dict = json.loads(content)
+
+        return output_dict
+
+
+
+    def classify_user_response(self, category_list, user_response):
+        """
+        Classify a user response into a category
+
+        Parameters
+        ----------
+        category_list : list
+            list of categories
+        user_response : str
+            user response
+
+        Returns
+        -------
+        llm_output : dict
+            output from the LLM
+        """
+
+        sys_prompt = f"""
+        You are a helpful text analyzer.
+
+        I will present you with a survey response text which contains one or more words.
+
+        Your task is to assign this text to one of the following categories:
+
+        {str(category_list)}
+
+        If none of the categories apply or the survey response text is too vague, then assign the text to a new category called 'Other'.
+
+        Return your response as a JSON object.
+
+        Examples:
+
+        If the survey response text is 'scale up', then return:
+
+        {{
+            result:
+                {{
+                    "response_text": "scale up"
+                    "category": "Manufacturing and Process Engineering"
+                }}
+        }}
+
+        If the survey response text is 'ability to keep up with and foresee research/industry trends and directions', then return:
+
+        {{
+            result:
+                {{
+                    "response_text": "ability to keep up with and foresee research/industry trends and directions"
+                    "category": "Business Acumen and Market Knowledge"
+                }}
+        }}
+
+        If the survey response text is 'language abilities (Chinese, Korean, Japanese)', then return:
+
+        {{
+            "category":
+                {{
+                    "response_text": "language abilities (Chinese, Korean, Japanese)"
+                    "category": "Language Skills and Multilingualism"
+                }}
+        }}
+        """
+
+        user_prompt =  f"""
+        Now return the result for the following survey response:
+
+        {str(user_response)}
+        """
+
+        response = self.client.chat.completions.create(
+                        model='gpt-4o-mini',
+                        messages=[
+                            {"role": "system", "content" : dedent(sys_prompt)},
+                            {"role": "user", "content" : dedent(user_prompt)}
+                        ],
+                        response_format={ "type": "json_object" }
+                    )
+
+        llm_output = json.loads(response.choices[0].message.content)
+
+        return llm_output
